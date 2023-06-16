8ccfa863733548ee4171bccff844f85cf3ab183a
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-16 12:29:51 -0700

    sd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,13 @@
  *
  * Descraption: print a qsd
  *	write
+ *	Return: 1(erroe)
 */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is usefu\l" - Dora Korpar, 2015 - 10 - 19\n";
+	char quo[] = "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,quo,59);
+	write(1, quo, 59);
 	return (1);
 }
