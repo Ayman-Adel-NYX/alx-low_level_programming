7ac3d98a7a9c4698359744003a613c95b577d5bb
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-16 11:21:53 -0700

    df

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
-int main (){
- 	puts("\"Programming is like building a multilingual puzzle"); 
+#include <stdio.h>
 
-	return 0;
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0(Success)
+ */
+
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
