ace12f2f8c526046c585a32d7fbeaa0d6b70c32d
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-19 08:23:22 -0700

    sd

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,8 +13,8 @@ int main(void)
 	char letter;
 	int n;
 
-	for (n = 0; num <= 9; n++)
-		putchar((num % 10) + '0');
+	for (n = 0; n <= 9; n++)
+		putchar((n % 10) + '0');
 	for (letter = 'a'; letter <= f; letter++)
 		putchar(letter);
 		putchar('\n');
