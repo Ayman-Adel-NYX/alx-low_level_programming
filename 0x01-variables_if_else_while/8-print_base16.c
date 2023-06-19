bd73c2b0fa5ccb9a8483a80084ca6b5ea36831e4
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-19 08:19:46 -0700

    sd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Descraption: fkf
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+	int n;
+
+	for (n = 0; num <= 9; n++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= f; letter++)
+		putchar(letter);
+		putchar('\n');
+
+		return (0);
+}
