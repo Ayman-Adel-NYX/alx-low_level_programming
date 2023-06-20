e1aabf8989e31d192738ce937367fe430fe891b7
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-20 09:51:16 -0700

    sd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,15 @@
 #include "main.h"
-
+/**
+ * jack_bauer - Prints every minute of the darting from 00:00 to 23:59.
+ */
 
 void jack_bauer(void)
 {
-	int n;
+	int n,m;
 
 	for (n = 0; n < 24; n++)
 	{
-		for (int m = 0; m < 60; m++)
+		for ( m = 0; m < 60; m++)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
