36a44af0f8e6b96b704f786b0dca71f1812aa31b
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-06-26 03:20:17 -0700

    df

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,15 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	int i = -1;
-	do {
-		i++;
+	int index = 0;
 
-		dest[i] = src[i];
-	} while (src[i] != '0');
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
 
 	return (dest);
 }
