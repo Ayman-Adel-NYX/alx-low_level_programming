516f028f11204bcc1bcf8ca92e4dbe0ec3affb7c
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-07-04 06:09:14 -0700

    sd

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
 /**
- * _memset - wwe
+ * _memset - fills memory with a constant byte .
+ * @s: allocated memory - array of characters .
+ * @b: char bytes .
+ * @n: number of spaces to fill .
  *
- * @s: sd
- * @b: sd
- * @n: sd
- *
- * Return: sd
+ * Return: pointer to memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
-
 	return (s);
 }
