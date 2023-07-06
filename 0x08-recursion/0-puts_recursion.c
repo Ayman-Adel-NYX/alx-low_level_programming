13ee9a8d956d389d5f388788f04b542095c098a5
Ayman-Adel-NYX <adel.ayman339@gmail.com>
2023-07-06 13:41:27 -0700

    sd

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  * _puts - dsd
+  *
+  * @s: sa
+  * Return: sd
+  */
+void _puts_recursion(char *s)
+{
+	if (*S)
+	{
+	_putchar(*S);
+	_puts_recursion(s + 1);
+	{
+		else	
+			_putchar('\n');
+}
